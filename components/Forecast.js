0d32cbf63a052959335fbfcb7d7dfa2367f97834
6210110097 <6210110097@psu.ac.th>
2021-07-12 11:17:16 +0700

    part4 notfinish

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function Forecast(props){
-
-    return(
-        <View>
+export default function Forecast(props) {
+    return (
+        <View >
             <Text>{props.main}</Text>
             <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <View>
+                <Text>{props.temp}</Text>
+                <Text>°C</Text>
         </View>
+    </View>
     );
-}
+   }   
--- components/Weather.js
+++ components/Weather.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
-import { ImageBackground, Text } from 'react-native'
+import { ImageBackground, Text, View } from 'react-native'
 import Forecast from './Forecast'
 
-export default function Weather(props){
-    const [forecastInfo, setForcecastInfo] = useState({
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
         temp: 0
-
     })
 
     return (
+    < View >
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-            <Forecast {...forecastInfo}/>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
         </ImageBackground>
-        
-    )
+    </View >
+ );
 }
 
 const styles = StyleSheet.create({
-    backdrop: {
-        width: '100%',
-        height: '100%'
-    }
-})
+ backdrop: {
+ alignItems: 'center',
+ width: '100%',
+ height: '100%'
+ },
+});
